09deb5707b60abd1ef7a30b93fd879dd6612e5cc
dim1112 <dim.1112.work@gmail.com>
2024-02-03 00:10:34 +0800

    feat : add functions isEnglish and parser for parsing departments

---
+++ src/crawler/crawparser.py
@@ -0,0 +1,38 @@
+from bs4 import BeautifulSoup as bs
+import re
+
+# Parse current page and return current language setting 
+def is_english(response):
+    html = bs(response.text, 'html.parser')
+    lang_tag = html.find_all('a', class_='intro lang')[0].text
+    return lang_tag!='English'
+
+# Function that parses the department page and returns a dictionary,
+# containing the department name, url to the department contained course page 
+# and the total number of courses in the department
+def parse_department(response):
+    html = bs(response.text, 'html.parser')
+    items = html.find_all('li')
+    results = []
+    for item in items:
+        dep_url = 'https://cis.ncu.edu.tw' + item.a.get("href")
+        dep_info_text = item.a.text.strip()
+        matches = re.findall('\((.*?)\)', dep_info_text)
+        if matches is not None:
+            # Potential bug: if cannot convert to int (Needs handle)
+            dep_courses = int(matches[-1])
+            dep_name = re.sub(f'\({dep_courses}\)', '', dep_info_text)
+        else :
+            print('Failed to parse department info when : ' + dep_info_text)
+            dep_courses = 'N/A'
+            dep_name = dep_info_text
+
+        dep_name = item.a.get_text()
+        results.append({
+            'name'   : dep_name,
+            'url'    : dep_url,
+            'courses': dep_courses,
+        })
+    return results
+
+
