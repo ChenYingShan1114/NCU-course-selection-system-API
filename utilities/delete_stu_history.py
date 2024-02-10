d2fcc80793eecfeda18139ef8b21d0b10a814de7
nightlan1015297 <nightlan1015297@users.noreply.github.com>
2024-02-10 12:35:25 +0000

    Update api data e0705899a3fc55c644ec7712f33baae631278c3a

--- utilities/delete_stu_history.py
+++
@@ -1,9 +0,0 @@
-import os 
-path = '../api/1122'
-for dirs in os.listdir(path):
-    print("current dir: ", dirs)
-    if '.json' not in dirs:
-        for file in os.listdir(f"{path}/{dirs}/history"):
-            if 'statistics' not in file:
-                print("removing: ", f"{path}/{dirs}/history/{file}")
-                os.remove(f"{path}/{dirs}/history/{file}")
